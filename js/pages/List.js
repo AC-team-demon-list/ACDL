24379714ac8a307276cd17a1bb5140851251b99d
niwoxgd-commits <niwoxgd@gmail.com>
2025-09-07 21:02:52 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -25,7 +25,7 @@ export default {
                 <table class="list" v-if="list">
                     <tr v-for="([level, err], i) in list">
                         <td class="rank">
-                            <p v-if="i + 1 <= 150" class="type-label-lg">#{{ i + 1 }}</p>
+                            <p v-if="i + 1 <= 75" class="type-label-lg">#{{ i + 1 }}</p>
                             <p v-else class="type-label-lg">Legacy</p>
                         </td>
                         <td class="level" :class="{ 'active': selected == i, 'error': !level }">
@@ -100,28 +100,28 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        pas de cheat svp et cbf c'est osef
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        que les membres de ac team send ok ?
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        des clicks et un bon record
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        ntm pop cat
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        hardest check pop cat
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        non popo stp fait po so
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        vive ac team
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        pop cat le roi
                     </p>
                 </div>
             </div>
